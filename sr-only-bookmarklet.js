f1564ecf4f8d85bd8220340cd1418cc1188e523a
Giovani Camara <Gcamara14@users.noreply.github.com>
2023-06-28 12:11:08 -0700

    Adds Xpath, CSS, renames columns

--- sr-only-bookmarklet.js
+++ sr-only-bookmarklet.js
@@ -1,4 +1,70 @@
 javascript:(function(){'use strict'
+// Start of code to display the xpath
+
+/**
+ * Get absolute xPath position from dom element
+ * xPath position will does not contain any id, class or attribute, etc selector
+ * Because, Some page use random id and class. This function should ignore that kind problem, so we're not using any selector
+ * 
+ * @param {Element} element element to get position
+ * @returns {String} xPath string
+ */
+ function getXPath(element) {
+    // Selector
+    let selector = '';
+    // Loop handler
+    let foundRoot;
+    // Element handler
+    let currentElement = element;
+
+    // Do action until we reach html element
+    do {
+        // Get element tag name 
+        const tagName = currentElement.tagName.toLowerCase();
+        // Get parent element
+        const parentElement = currentElement.parentElement;
+
+        // Count children
+        if (parentElement.childElementCount > 1) {
+            // Get children of parent element
+            const parentsChildren = [...parentElement.children];
+            // Count current tag 
+            let tag = [];
+            parentsChildren.forEach(child => {
+                if (child.tagName.toLowerCase() === tagName) tag.push(child) // Append to tag
+            })
+
+            // Is only of type
+            if (tag.length === 1) {
+                // Append tag to selector
+                selector = `/${tagName}${selector}`;
+            } else {
+                // Get position of current element in tag
+                const position = tag.indexOf(currentElement) + 1;
+                // Append tag to selector
+                selector = `/${tagName}[${position}]${selector}`;
+            }
+
+        } else {
+            //* Current element has no siblings
+            // Append tag to selector
+            selector = `/${tagName}${selector}`;
+        }
+
+        // Set parent element to current element
+        currentElement = parentElement;
+        // Is root  
+        foundRoot = parentElement.tagName.toLowerCase() === 'html';
+        // Finish selector if found root element
+        if(foundRoot) selector = `/html${selector}`;
+    }
+    while (foundRoot === false);
+
+    // Return selector
+    return selector;
+}
+
+// Start of regular JS Code to display the table  	
 function listThingsWithTitles(){
   console.clear();
   function insertAfter(newNode, referenceNode) {
@@ -99,6 +165,9 @@ function listThingsWithTitles(){
     }
   
     let tc=elWithAriaLabel.textContent;
+
+    // Get the xpath
+    let xPathLocation=getXPath(elWithAriaLabel);    
   
     if (elWithAriaLabel.tagName==='IMG') {
       tc=elWithAriaLabel.getAttribute('alt');
@@ -133,7 +202,7 @@ function listThingsWithTitles(){
         warn=false;
         err=true;
         identCount++;
-        notes+='- Remember to translate this text!<br>';
+        notes+='- Remember to translate the visually hidden text (Yellow Column)!<br>';
       }
     }
 
@@ -145,14 +214,14 @@ function listThingsWithTitles(){
     }
     row+='>';
     row+='<td>' + tc + '</td>';
-    row+='<td>' + ariaLabelTextValue + '</td>';
+	row+='<td><div class="snippet"><label for="xpath'+ i + '">xPath</label><textarea readonly id="xpath'+ i + '" aria-label="Xpath snippet for this node">' + xPathLocation + '</textarea></div></td>';        
+    row+='<td><div class="snippet"><label for="snip'+ i + '">Code Reference</label><textarea readonly id="snip'+ i + '" aria-label="Markup snippet for this node">' + snippet + '</textarea></div></td>';
+	row+='<td>' + ariaLabelTextValue + '</td>';    
     row+='<td>';
     if (err) {
       row+='<div class="issues">⚠️ Warning:</div>';
     }
-    consoleOutput='Element with title \'' + tc.trim() + '\':\n'+notes+'Markup with issue:\n'+snippet+'\n---------------\n';
-    row+=notes + '<br></td>';
-    row+='<td><div class="snippet"><label for="snip'+ i + '">Code snippet (Expand)</label><textarea id="snip'+ i + '" aria-label="Markup snippet for this node">' + snippet + '</textarea></div></td>';
+    row+=notes + '<br></td>';    
     row+='</tr>';
     i++;
     if (warn || err) {
@@ -163,8 +232,8 @@ function listThingsWithTitles(){
       console.log(consoleOutput);
     }
   });
-  s='<style>[aria-pressed=true]{color:white;background:rebeccapurple;}div.issues{font-weight:bold;};textarea {margin:5px 0;}.snippet label {font-weight:bold;font-size:0.8em;color:black;}.snippet{background:#efefef;outline:1px solid #666;padding:5px;margin-top:5px;}.checkDiffs{background:PapayaWhip;}.checkDiffs:after{content:"Accessible name differs";color:#a50202;font-weight:bold;font-size:10px;display:block}.warn {background:lightyellow;}.err {background:PapayaWhip;color:#a50202;}.visually-hidden,.a11y,.visuallyhidden,.sr-text,.sr-only {clip-path: inset(100%);clip: rect(1px, 1px, 1px, 1px);height: 1px;overflow: hidden;position: absolute;white-space: nowrap;width: 1px;}* {-webkit-box-sizing: border-box;box-sizing: border-box;}html {/*border: .75em solid #fff;*/min-height: 100vh;}body {background: #f7f7f5;color: #333;font: 400 105%/1.4 "Work Sans", sans-serif;margin: 1.5em auto;max-width: 54em;width: 90%;}a:elWithAriaLabel,a:visited {border-bottom: 1px solid rgba(42, 122, 130, .5);color: #2b7a82;text-decoration: none;}a:hover {border-bottom: 2px solid;color: #1e565c;}button:focus,a:focus {box-shadow: none;outline-offset: 2px;outline: 3px solid rgba(42, 122, 130, .75);}a:focus {border-bottom: none;}a:active {background: #333;color: #fff;}code {font-family: Consolas, monaco, monospace;-moz-tab-size: 4;tab-size: 4;text-transform: none;white-space: pre-wrap;color:brown;}textarea {width: 100%}legend h2, legend h3 {margin: 0;}table {border-collapse: collapse;}th,td {padding: 10px;border:2px solid #2b7a82;}table caption {font-weight: bold;text-align: left;margin:1em 0;}</style><h1>List of elements with visually hidden text on the page</h1>';
-  s+='<table border="1" cellpadding="5"><caption>Visually hidden text affects accessibility and must be translated. It is invisible text just for screen readers, hence why it is hidden. Remember to let the entire page load before running this tool. Refer to the LD-Visually-Hidden Utility</caption><thead><tr valign=top><th scope="col">Visually Hidden text</th><th>CSS Class</th><th scope="col">Notes</th><th>Code Markup</th></tr></thead><tbody>' + row + '</tbody></table>';
+  s='<style>[aria-pressed=true]{color:white;background:rebeccapurple;}div.issues{font-weight:bold;};textarea {margin:5px 0;}.snippet label {font-weight:bold;font-size:0.8em;color:black;}.snippet{background:#efefef;outline:1px solid #666;padding:5px;margin-top:5px;}.checkDiffs{background:PapayaWhip;}.checkDiffs:after{content:"Accessible name differs";color:#a50202;font-weight:bold;font-size:10px;display:block}.warn {background:lightyellow;}.err {background:PapayaWhip;color:#a50202;}.visually-hidden,.a11y,.visuallyhidden,.sr-text,.sr-only {clip-path: inset(100%);clip: rect(1px, 1px, 1px, 1px);height: 1px;overflow: hidden;position: absolute;white-space: nowrap;width: 1px;}* {-webkit-box-sizing: border-box;box-sizing: border-box;}html {/*border: .75em solid #fff;*/min-height: 100vh;}body {background: #f7f7f5;color: #333;font: 400 105%/1.4 "Work Sans", sans-serif;margin: 1.5em auto;max-width: 54em;width: 90%;}a:elWithAriaLabel,a:visited {border-bottom: 1px solid rgba(42, 122, 130, .5);color: #2b7a82;text-decoration: none;}a:hover {border-bottom: 2px solid;color: #1e565c;}button:focus,a:focus {box-shadow: none;outline-offset: 2px;outline: 3px solid rgba(42, 122, 130, .75);}a:focus {border-bottom: none;}a:active {background: #333;color: #fff;}code {font-family: Consolas, monaco, monospace;-moz-tab-size: 4;tab-size: 4;text-transform: none;white-space: pre-wrap;color:brown;}textarea {width: 100%}legend h2, legend h3 {margin: 0;}table {border-collapse: collapse;}th,td {padding: 10px;border:2px solid #2b7a82;}table caption {font-weight: bold;text-align: left;margin:1em 0;}th{min-width: 200px;} table td:nth-child(1), table th:nth-child(1){ background: yellow; font-weight: bold; color: #000000;}</style><h1>List of CSS visually hidden text on the page</h1>';
+  s+='<table border="1" cellpadding="5"><caption>Visually hidden text affects accessibility and must be translated. It is invisible text that is hidden using CSS just for screen readers, hence why it is hidden. Remember to let the entire page load before running this tool. Refer to the LD-Visually-Hidden Utility. Use the xPath to translate.</caption><thead><tr valign=top><th scope="col">Visually Hidden text (Translate)</th><th scope="col">xPath</th><th>Code OuterHTML</th><th>CSS Class</th><th>Notes</th></tr></thead><tbody>' + row + '</tbody></table>';
   s+='<script>function showElsWithSkipLinkClasses(){';
   s+='var refWindow=window.opener;';
   s+='}window.addEventListener("load", (event) => {showElsWithSkipLinkClasses();});</script>';
